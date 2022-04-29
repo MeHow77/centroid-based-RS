3827669520f6df1ed328df82c02b24e7141476a2
Mehow <michal.hawa@gmail.com>
2022-04-29 14:28:48 +0200

    first changes

---
+++ crop_dataset.py
@@ -0,0 +1,36 @@
+from calendar import c
+import json
+
+subsets = ['train', 'test', 'val', 'query', 'gallery']
+#subsets = ['train']
+min_prcnt = 0.1
+size = 256
+for subset in subsets:
+    f = open(f'datasets/df/full_json/{subset}_reid_cropped_{size}_{size}.json')
+    data = json.load(f)
+    
+    min_records = int(len(data['images'])*min_prcnt)
+    print(f"Min records to obtain: {min_records}")
+    curr_record = 0
+    if_still_current_item = True
+    cropped_dataset = {'images':[], 'annotations': []}
+    
+    # take another record until it exceeds needed number or  it still holds to the previous record
+    while curr_record <= min_records or if_still_current_item:
+        # get current record
+        sample_item = data['images'][curr_record]
+        sample_anno = data['annotations'][curr_record]
+        next_sample = data['images'][curr_record+1]
+        cropped_dataset['images'].append(sample_item)
+        cropped_dataset['annotations'].append(sample_anno)
+        # take 'consumer' or 'shop' factor for next record
+        next_item_id = next_sample['file_name'].split('_')[1]
+        next_id = sample_item['file_name'].split('_')[1]
+        # create True flag for consumer
+        if_still_current_item = True if next_item_id == next_id else False
+        curr_record += 1
+        
+    # print(cropped_dataset['images'])
+    # print(cropped_dataset['annotations'])
+    with open(f'datasets/df/cropped_json/{subset}_reid_cropped_{size}_{size}.json', 'w') as json_f:
+       json.dump(cropped_dataset, json_f)
--- test_scripts/test_ctl_model_l_r50ibn_df1.sh
+++ test_scripts/test_ctl_model_l_r50ibn_df1.sh
@@ -1,12 +1,14 @@
 python3 train_ctl_model.py \
---config_file="configs/320_resnet50_ibn_a.yml" \
+--config_file="configs/320_resnet50_ibn_a_119epoch.yml" \
 GPU_IDS "[0]" \
 DATASETS.NAMES 'df1' \
 DATASETS.JSON_TRAIN_PATH 'datasets/df/full_json/train_reid_cropped_320_320.json' \
 DATASETS.ROOT_DIR 'datasets/df/320_320_cropped_images' \
-SOLVER.IMS_PER_BATCH "2" \
-TEST.IMS_PER_BATCH "64" \
+SOLVER.IMS_PER_BATCH "12" \
+TEST.IMS_PER_BATCH "2" \
 SOLVER.BASE_LR "1e-4" \
 OUTPUT_DIR '.output-dir' \
 DATALOADER.USE_RESAMPLING "False" \
-MODEL.KEEP_CAMID_CENTROIDS "False" 
+MODEL.KEEP_CAMID_CENTROIDS "False" \
+MODEL.PRETRAIN_PATH "models/centroids_df_resnet_50_ibn_a_checkpoint_119.pth" \
+TEST.ONLY_TEST "True" 
